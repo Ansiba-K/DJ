9a3e10e58035622863c622c591010ae65b971de5
Ansiba Kandankulavan <91192566+Ansiba-K@users.noreply.github.com>
2026-08-16 09:58:54 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,20 @@
+from django.http import JsonResponse, HttpResponse
+from django.shortcuts import render
+from .models import Student
+from .serializer import StudentSerializer
+from rest_framework import status
+from rest_framework.decorators import api_view
+
+
+@api_view(['GET', 'POST'])
+def index(request):
+    if request.method == 'GET':
+      students = Student.objects.all()
+      serializer = StudentSerializer(students, many=True) 
+      return JsonResponse(serializer.data, safe=False)
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+    return JsonResponse({'error': 'Invalid request method'}, status=405)
