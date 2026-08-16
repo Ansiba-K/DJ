99ed99001c1c25a7133811c3784f9def185a3c34
Ansiba Kandankulavan <ansiansiba7@gmail.com>
2026-08-16 10:04:53 +0530

    Revert "updated views.py"
    This reverts commit 9794def8b93d94635ce44bbb48396aca134de2b6.

--- views.py
+++ views.py
@@ -18,7 +18,3 @@ def index(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
     return JsonResponse({'error': 'Invalid request method'}, status=405)
-
-
-def login(request):
-    pass
